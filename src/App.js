43d4e07724f4cda3b020b735eff8f734928c9810
ryan.gravener <ryan.gravener@angi.com>
2024-02-29 10:39:33 -0500

    initial import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactCompareSlider, ReactCompareSliderImage, useReactCompareSliderRef } from 'react-compare-slider';
+import React from 'react';
 
-function App() {
+const App = () => {
+
+  const reactCompareSliderRef = useReactCompareSliderRef();
+
+  const [rate, setRate] = React.useState(24);
+
+  let percent = .9
+  let direction = 1
+  let increment = 0.01
+
+  setInterval(() => {
+    let incr = increment
+    if (direction == -1) {
+      incr = -incr
+    }
+    const newPercent = percent + incr
+    let dir = 1
+    let per = 1
+    if (newPercent <= 0.0 && direction == -1) {
+      dir = 1
+      per = 0
+    } else if (newPercent >= 1.0 && direction == 1) {
+      dir = -1
+      per = 1
+    } else {
+
+      per = newPercent
+      dir = direction
+    }
+    percent = per
+    direction = dir
+    reactCompareSliderRef.current.setPosition(per * 100)
+  }, rate);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ width: "50%", height: "50%" }}>
+      <ReactCompareSlider
+        ref={reactCompareSliderRef}
+        transition="ease-in-out"
+        itemOne={<ReactCompareSliderImage src={"image1.jpeg"} alt="Image one" />}
+        itemTwo={<ReactCompareSliderImage src={"image2.jpeg"} alt="Image one" />}
+      /></div>
   );
 }
 
 export default App;
+
